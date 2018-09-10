ef3263ea1308cecee1006d307d56f8bcf934c53b
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-09-10 14:31:31 +0900

    remove un-used code in App.jhs

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { StyleSheet, View, StatusBar } from 'react-native';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import AppReducer from './src/reducers';
 import { AppNavigator, middleware } from './src/navigators/AppNavigator';
 // import SampleComponent from './src/components/SampleComponent';
-
 
 const store = createStore(AppReducer, applyMiddleware(middleware));
 
