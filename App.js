548a597efd07fd4a9efbb4abb16bb801f630de85
nourbalaha <nourbalaha0@gmail.com>
2018-09-11 13:45:37 +0900

    wip: working on navigation

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Analytics from './src/components/Analytics';
-import { insert, push, select, update, remove } from './db';
-import { migrate } from './migrate';
-import { AppNavigator, middleware } from './src/navigators/AppNavigator';
-import {store} from './src/store'
+import { AppNavigator } from './src/navigators/AppNavigator';
+import { store } from './src/store';
 
 const App = () => (
   <Provider store={store}>
-    {/* <Analytics /> */}
     <AppNavigator />
   </Provider>
 );
--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.apiKey,
   authDomain: 'test-d3d42.firebaseapp.com',
   databaseURL: 'https://test-d3d42.firebaseio.com',
   projectId: 'test-d3d42',
@@ -12,65 +12,64 @@ const config = {
 export const app = firebase.initializeApp(config);
 
 // insert
-export const insert = (ref,data)=>{
-    firebase
-      .database()
-      .ref(ref)
-      .set(data)
-      .then(() => {
-        console.log('Inserted');
-      })
-      .catch(error => {
-        console.log(error);
-      });
-}
+export const insert = (ref, data) => {
+  firebase
+    .database()
+    .ref(ref)
+    .set(data)
+    .then(() => {
+      console.log('Inserted');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
 // Push
-export const push = (ref,data)=>{
-    firebase
-      .database()
-      .ref(ref)
-      .push(data)
-      .then(() => {
-        console.log('Pushed');
-      })
-      .catch(error => {
-        console.log(error);
-      });
-}
+export const push = (ref, data) => {
+  firebase
+    .database()
+    .ref(ref)
+    .push(data)
+    .then(() => {
+      console.log('Pushed');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
 // select
-export const select = (ref)=>{
-firebase
-  .database()
-  .ref(ref)
-  .on('value', data => {
-    return data.toJSON();
-  }).then((data)=>{
-    console.log(data);
-  });
-}
+export const select = ref => {
+  firebase
+    .database()
+    .ref(ref)
+    .on('value', data => data.toJSON())
+    .then(data => {
+      console.log(data);
+    });
+};
 // update
-export const update = (ref,data)=>{
-firebase
-  .database()
-  .ref(ref)
-  .update(data)
-  .then(() => {
-    console.log('Updated');
-  })
-  .catch(error => {
-    console.log(error);
-  });
-}
+export const update = (ref, data) => {
+  firebase
+    .database()
+    .ref(ref)
+    .update(data)
+    .then(() => {
+      console.log('Updated');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
 // delete
-export const remove = (ref)=>{
-firebase
-  .database()
-  .ref(ref)
-  .remove()
-  .then(() => {
-    console.log('Deleted');
-  })
-  .catch(error => {
-    console.log(error);
-  });
-}
+export const remove = ref => {
+  firebase
+    .database()
+    .ref(ref)
+    .remove()
+    .then(() => {
+      console.log('Deleted');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
