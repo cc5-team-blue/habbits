8f617bf0b7ff36964fcb9e1291b2a36afafabc66
Tsuyoshi Akiyama <ka090824@gmail.com>
2018-09-13 18:18:10 +0900

    Link react native with svg library

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './App';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 AppRegistry.registerComponent('habbits', () => App);
