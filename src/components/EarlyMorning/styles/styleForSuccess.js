0b9b2add037a0e07fd9ba2626447d176fd0d6ad7
Nour Balaha <nourbalaha0@gmail.com>
2018-09-15 18:39:03 +0900

    feat added styles to EarlyMorning

---
+++ src/components/EarlyMorning/styles/styleForSuccess.js
@@ -0,0 +1,85 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  realContainer: {
+    height: '100%',
+    backgroundColor: '#3B495B',
+  },
+  container: {
+    marginLeft: '6%',
+    marginTop: '12%',
+    height: '90%',
+    width: '88%',
+  },
+  awesomeWrapper: {
+    paddingTop: 0,
+    marginBottom: '10%',
+  },
+  youAreText: {
+    width: 280,
+    height: 29.5,
+    fontFamily: 'Futura',
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    letterSpacing: 0,
+    textAlign: 'center',
+    color: 'rgba(59,73,91, 0.68)',
+  },
+  pointsText: {
+    fontFamily: 'Futura',
+    fontSize: 18,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    textAlign: 'center',
+    color: 'rgba(59, 73, 91, 0.68)',
+  },
+  challengeText: {
+    fontFamily: 'Futura',
+    fontSize: 25,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    textAlign: 'center',
+    color: '#3b495b',
+  },
+  awesomeText: {
+    textAlign: 'center',
+    fontFamily: 'Futura',
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    color: '#3B495B',
+  },
+  happyRabbitImage: {
+    width: 155,
+    height: 262,
+    marginBottom: '12%',
+  },
+  yayImgContainer: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '95%',
+    backgroundColor: '#ffe5d8',
+  },
+  yayButton: {
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    backgroundColor: '#ff627a',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '10%',
+  },
+  yayText: {
+    fontFamily: 'Futura',
+    fontSize: 25,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    letterSpacing: 0,
+    color: '#ffffff',
+  },
+});
+
+export default styles;
