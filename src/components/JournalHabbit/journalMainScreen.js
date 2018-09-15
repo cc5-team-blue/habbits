3b78c60c02841a3fbaeeccf3d23f4d148cf6be68
m0m0i <hirocorp01@gmail.com>
2018-09-15 21:15:16 +0900

    Added TextInput

--- src/components/JournalHabbit/journalMainScreen.js
+++ src/components/JournalHabbit/journalMainScreen.js
@@ -1,33 +1,62 @@
-import React from 'react';
-import { Text, View, StatusBar } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StatusBar, TextInput } from 'react-native';
 import { connect } from 'react-redux';
 import { NavigationActions } from 'react-navigation';
+import styles from '../../css/styleForJournal';
 
-export const journalDescription = ({ goToJournalSuccess }) => (
-  <View>
-    <StatusBar barStyle="light-content" />
-    <View>
+export class journalDescription extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      greatful: '',
+      til: '',
+    };
+  }
+
+  render() {
+    const { goToJournalSuccess } = this.props;
+    const { greatful, til } = this.state;
+    return (
       <View>
-        <Text>Hi Hiro, how was your day?</Text>
+        <StatusBar barStyle="light-content" />
+        <View>
+          <View>
+            <Text>Hi Hiro, how was your day?</Text>
+          </View>
+          <View>
+            <Text>Rate your day:</Text>
+            {/* Rating star component */}
+          </View>
+          <View>
+            <Text>One thing you&#039;re grateful for:</Text>
+            <TextInput
+              placeholder="Grateful for..."
+              autoCapitalize="none"
+              style={styles.textInput}
+              onChangeText={text => this.setState({ greatful: text })}
+              value={greatful}
+              returnKeyType="next"
+            />
+          </View>
+          <View>
+            <Text>Something you&#039;ve learned today:</Text>
+            <TextInput
+              placeholder="Today I learned..."
+              autoCapitalize="none"
+              style={styles.textInput}
+              onChangeText={text => this.setState({ til: text })}
+              value={til}
+              returnKeyType="next"
+            />
+          </View>
+        </View>
+        <View onTouchStart={goToJournalSuccess}>
+          <Text>Submit!</Text>
+        </View>
       </View>
-      <View>
-        <Text>Rate your day:</Text>
-        {/* Rating star component */}
-      </View>
-      <View>
-        <Text>One thing you&#039;re grateful for:</Text>
-        {/* Text input component */}
-      </View>
-      <View>
-        <Text>Something you&#039;ve learned today:</Text>
-        {/* Text input component */}
-      </View>
-    </View>
-    <View onTouchStart={goToJournalSuccess}>
-      <Text>Submit!</Text>
-    </View>
-  </View>
-);
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   goToJournalSuccess: () => {
---
+++ src/css/styleForJournal.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  textInput: {
+    width: 270,
+    height: 45.5,
+    borderRadius: 10,
+    backgroundColor: '#ffffff',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#151515',
+    marginTop: 7,
+    marginBottom: 7,
+    padding: 12.5,
+  },
+});
+
+export default styles;
