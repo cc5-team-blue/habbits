d0a09bc9a46da4df9dc867b84107fdd4965e6e44
m0m0i <hirocorp01@gmail.com>
2018-09-18 21:05:36 +0900

    Separate styling

--- src/auth/SignUp.js
+++ src/auth/SignUp.js
@@ -1,86 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, StatusBar } from 'react-native';
+import { Text, TextInput, View, StatusBar } from 'react-native';
 
 import { app } from '../../db';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#3b495b',
-  },
-  signupContainer: {
-    width: 325,
-    height: 600,
-    marginTop: 13,
-    borderRadius: 10,
-    backgroundColor: '#fbfeff',
-  },
-  signupWrapper: {
-    marginLeft: 25,
-    marginRight: 25,
-    marginTop: 31,
-  },
-  headline: {
-    width: 267,
-    height: 90,
-    fontFamily: 'Futura',
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#242733',
-  },
-  errorWrapper: {
-    height: 15,
-  },
-  textInput: {
-    width: 270,
-    height: 45.5,
-    borderRadius: 10,
-    backgroundColor: '#ffffff',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderColor: '#151515',
-    marginTop: 7,
-    marginBottom: 7,
-    padding: 12.5,
-  },
-  signupButton: {
-    marginTop: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 270,
-    height: 35,
-    borderRadius: 10,
-    backgroundColor: '#ff575c',
-  },
-  signupText: {
-    width: 186.9,
-    height: 22,
-    fontFamily: 'Futura',
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#ffffff',
-  },
-  switchSignIn: {
-    marginTop: 10,
-    height: 30,
-    width: '100%',
-    justifyContent: 'center',
-  },
-  switchSignInText: {
-    width: '100%',
-    height: 20.5,
-    fontFamily: 'Avenir',
-    fontSize: 15,
-    textAlign: 'right',
-    color: '#3b495b',
-  },
-  bold: {
-    fontWeight: 'bold',
-  },
-});
+import styles from '../css/styleForAuth';
 
 export default class SignUp extends React.Component {
   constructor(props) {
