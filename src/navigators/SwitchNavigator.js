f2915e2004e40dfb5b58375c232604a97de0cc3c
m0m0i <hirocorp01@gmail.com>
2018-09-22 18:21:10 +0900

    Added JournalEntries component

--- src/navigators/SwitchNavigator.js
+++ src/navigators/SwitchNavigator.js
@@ -11,6 +11,7 @@ import journalFailure from '../components/JournalHabbit/journalFailure';
 import journalMainScreen from '../components/JournalHabbit/journalMainScreen';
 import journalSuccess from '../components/JournalHabbit/journalSuccess';
 import journalSuccessBIG from '../components/JournalHabbit/JournalSuccessBIG';
+import JournalEntries from '../components/JournalEntries/JournalEntries';
 import MainNavigator from './MainNavigator';
 import EarlyMorningNavigator from '../components/EarlyMorning/nav/Navigator';
 import FailureMinus from '../components/SleepHabbit/FailureMinus';
@@ -31,6 +32,7 @@ const switchNav = createSwitchNavigator(
     JournalMainScreen: { screen: journalMainScreen },
     JournalSuccess: { screen: journalSuccess },
     JournalSuccessBIG: { screen: journalSuccessBIG },
+    JournalEntries: { screen: JournalEntries },
     Authentication: { screen: MainNavigator },
     EarlyMorning: { screen: EarlyMorningNavigator },
     FailureMinus: { screen: FailureMinus },
