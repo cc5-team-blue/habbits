341fc07d2b97e80295508e3ea8b73869189b6796
Code Chrysalis <codechrysalis@Codes-Mac-mini.local>
2018-09-24 20:08:05 +0900

    cleanup

--- src/components/Main.js
+++ src/components/Main.js
@@ -23,8 +23,6 @@ class Main extends Component {
     getTotalPointsFromFB(uid);
   }
 
-  // 3
-
   // Remove listeners allocated in createNotificationListeners()
   componentWillUnmount() {
     this.notificationListener();
@@ -59,7 +57,6 @@ class Main extends Component {
     }
   };
 
-  // 2
   requestPermission = async () => {
     try {
       await firebase.messaging().requestPermission();
@@ -72,18 +69,14 @@ class Main extends Component {
   };
 
   async createNotificationListeners() {
-    /*
-    * Triggered when a particular notification has been received in foreground
-    * */
+    // Triggered when a particular notification has been received in foreground
     this.notificationListener = firebase.notifications().onNotification(notification => {
       const { title, body } = notification;
       const { showAlert } = this.props;
       showAlert(title, body);
     });
 
-    /*
-    * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
-    * */
+    // If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
     this.notificationOpenedListener = firebase
       .notifications()
       .onNotificationOpened(notificationOpen => {
@@ -92,21 +85,17 @@ class Main extends Component {
         showAlert(title, body);
       });
 
-    /*
-    * If your app is closed, 
-    * you can check if it was opened by a notification being clicked / tapped / opened as follows:
-    * */
+    // If your app is closed,
+    // you can check if it was opened by a notification being clicked / tapped / opened as follows:
     const notificationOpen = await firebase.notifications().getInitialNotification();
     if (notificationOpen) {
       const { title, body } = notificationOpen.notification;
       const { showAlert } = this.props;
       showAlert(title, body);
     }
-    /*
-    * Triggered for data only payload in foreground
-    * */
+
+    // Triggered for data only payload in foreground
     this.messageListener = firebase.messaging().onMessage(message => {
-      // process data message
       console.log(JSON.stringify(message));
     });
   }
