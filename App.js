85b569e4c7d05bf2d1701390a1f404c97ef1972c
Code Chrysalis <codechrysalis@Codes-Mac-mini.local>
2018-09-24 20:21:28 +0900

    cleanup

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-// import { migrate } from './migrate';
 import { AppNavigator } from './src/navigators/AppNavigator';
-// import AuthNavigator from './src/navigators/AuthNavigator';
-// import EarlyMorningNavigator from './src/components/EarlyMorning/nav/Navigator';
 
 import store from './src/store';
-
-// migrate();
 
 const App = () => (
   <Provider store={store}>
     <AppNavigator />
-    {/* <EarlyMorningNavigator /> */}
-    {/* <AuthNavigator /> */}
   </Provider>
 );
 
